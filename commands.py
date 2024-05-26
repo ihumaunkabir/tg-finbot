b4b57ea85784bbc80ec6ed904f58388919f12e27
Humaun Kabir <ask.humaunkabir@gmail.com>
2024-05-26 19:49:39 +0300

    Refactor strings

--- commands.py
+++ commands.py
@@ -2,10 +2,10 @@ from telegram import Update
 from telegram.ext import ContextTypes
 
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text('Hello from start button, start your conversation')
+    await update.message.reply_text('Welcome the bot, start your conversation by uploading file, image or send text.')
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text('Browse help.')
+    await update.message.reply_text('You can have datapoints from a pdf/image easily at your hands.')
 
 async def info_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text('Get information at your convenience.')
