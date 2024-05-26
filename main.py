b85f38d4ea2e6e8f5504990e7450b7b7a6199dec
Humaun Kabir <ask.humaunkabir@gmail.com>
2024-05-26 19:50:41 +0300

    Refactor main.py

--- main.py
+++ main.py
@@ -10,23 +10,6 @@ from telegram.ext import Application, CommandHandler, MessageHandler, filters, C
 async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
     print(f'Update {update} cause error {context.error}')
 
-async def process_image(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    file_id = update.message.photo[-1].file_id
-    file = await context.bot.get_file(file_id)
-    #await file.download_to_drive()
-    flink = file.file_path
-    resptext: str = handle_file(flink)
-    await update.message.reply_text(resptext)
-   # await update.message.reply_text(handle_file)
-
-async def process_file(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    file_id = update.message.document.file_id  # Change to audio, video, etc., based on the file type
-    file = await context.bot.get_file(file_id)
-    #await file.download_to_drive()
-    flink = file.file_path
-    # print(flink)
-    resptext: str = handle_file(flink)
-    await update.message.reply_text(resptext)
 
 if __name__ == '__main__':
     print('Bot started...')
