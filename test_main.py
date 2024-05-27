58e78164c0a713f7ef6c873263dcbeb018efbb7d
Humaun Kabir <ask.humaunkabir@gmail.com>
2024-05-27 18:38:54 +0300

    Make empty test

--- test_main.py
+++ test_main.py
@@ -1,23 +1,7 @@
 import unittest
 
 class TestMain(unittest.TestCase):
-    def test_start_command(self):
-        pass
-
-    def test_help_command(self):
-        pass
-
-    def test_info_command(self):
-        pass
-
-    def test_handle_message(self):
-        pass
-
-    def test_process_image(self):
-        pass
-
-    def test_process_file(self):
-        pass
+    pass
 
 if __name__ == '__main__':
     unittest.main()
